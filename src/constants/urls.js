73c63066173f845aa25388d067fd265137efa092
KyzAlexander <kyzkasan@gmail.com>
2023-09-23 11:38:11 +0300

    correct api

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,6 @@
 export const BASE_URL_WEATHER = "http://api.openweathermap.org/data/2.5/";
 export const BASE_URL_CITY = "http://api.openweathermap.org/geo/1.0/";
-export const API_KEY_WEATHER = process.env.REACT_APP_API_KEY_WEATHER;
-export const API_KEY_MAP = process.env.REACT_APP_API_KEY_MAP;
+// export const API_KEY_WEATHER = process.env.REACT_APP_API_KEY_WEATHER;
+// export const API_KEY_MAP = process.env.REACT_APP_API_KEY_MAP;
+export const API_KEY_WEATHER = "183d9ffc6524fdd4644da58e5ced1ce3";
+export const API_KEY_MAP = "15c7d273-a9ab-44d6-be3c-63af9e8e6e0e";
