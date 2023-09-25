b7e58249109793bb413707354738430f4dfc576d
KyzAlexander <93609052+KyzAlexander@users.noreply.github.com>
2023-09-25 09:26:45 +0300

    Update urls

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
-export const BASE_URL_WEATHER = "http://api.openweathermap.org/data/2.5/";
-export const BASE_URL_CITY = "http://api.openweathermap.org/geo/1.0/";
+export const BASE_URL_WEATHER = "https://api.openweathermap.org/data/2.5/";
+export const BASE_URL_CITY = "https://api.openweathermap.org/geo/1.0/";
 // export const API_KEY_WEATHER = process.env.REACT_APP_API_KEY_WEATHER;
 // export const API_KEY_MAP = process.env.REACT_APP_API_KEY_MAP;
 export const API_KEY_WEATHER = "183d9ffc6524fdd4644da58e5ced1ce3";
